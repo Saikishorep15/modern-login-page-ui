7a568b851a28e63893c571a3de645f39a38e0085
Sai kishore P <saikishor143p@gmail.com>
2026-03-01 22:53:35 +0530

    Add login form validation script

---
+++ Script.js
@@ -0,0 +1,12 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if(email === "admin@example.com" && password === "123456") {
+        alert("Login Successful ✅");
+    } else {
+        alert("Invalid Email or Password ❌");
+    }
+});
